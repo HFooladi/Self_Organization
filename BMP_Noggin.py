a301f89e00c1251a2b2d2937427ba76ee5f9e27c
Hosein Fooladi <fooladi.hosein@gmail.com>
2020-11-24 14:01:37 +0330

    formatting the code with yapf

--- BMP_Noggin.py
+++ BMP_Noggin.py
@@ -1,8 +1,7 @@
-
 # coding: utf-8
 
-# In[1]:
-
+__author__ = "Hosein Fooladi"
+__email__ = "fooladi.hosein@gmail.com"
 
 import numpy as np
 import scipy.integrate
@@ -11,24 +10,27 @@ import scipy.optimize
 # Plotting modules
 import matplotlib.pyplot as plt
 import seaborn as sns
-colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
-          '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
-          '#bcbd22', '#17becf']
-sns.set(style='whitegrid', context='notebook', palette=colors, 
+colors = [
+    '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2',
+    '#7f7f7f', '#bcbd22', '#17becf'
+]
+sns.set(style='whitegrid',
+        context='notebook',
+        palette=colors,
         rc={'axes.labelsize': 16})
 
 #get_ipython().magic('matplotlib inline')
 #get_ipython().magic("config InlineBackend.figure_formats = {'png', 'retina'}")
 
 
-# In[4]:
-
-
 def BMP4_Noggin(ab, t, r1, r2, beta_b, beta_n, lambd, n, a_b, a_n):
-    """Right hand side of ODEs for activator-repressor clock."""
-    nog, bmp = ab
-    return np.array([lambd *(beta_n * r2 * bmp**n / (1 + r2 * bmp**n) - nog + a_n), 
-                     beta_b * bmp**n / (1 + nog**n + bmp**n + r1 * nog**n * bmp**n) - bmp + a_b])
+  """Right hand side of ODEs for activator-repressor clock."""
+  nog, bmp = ab
+  return np.array([
+      lambd * (beta_n * r2 * bmp**n / (1 + r2 * bmp**n) - nog + a_n),
+      beta_b * bmp**n / (1 + nog**n + bmp**n + r1 * nog**n * bmp**n) - bmp + a_b
+  ])
+
 
 '''
 For Monotic solution, you can use these parameters and by doing this you can reproduce fig3.A
@@ -62,29 +64,23 @@ ab = scipy.integrate.odeint(BMP4_Noggin, ab0, t, args=args)
 
 #print(ab[:,1])
 # Plot
-plt.plot(t, ab[:,0], '--')
-plt.plot(t, ab[:,1])
+plt.plot(t, ab[:, 0], '--')
+plt.plot(t, ab[:, 1])
 plt.xlabel('t')
 plt.ylabel('Noggin, BMP4')
-plt.legend(('Noggin', 'BMP4'));
+plt.legend(('Noggin', 'BMP4'))
 plt.show()
-
-
-# In[6]:
 
 
 # Plot
-plt.plot(ab[:,0], ab[:,1], color='gray')
+plt.plot(ab[:, 0], ab[:, 1], color='gray')
 plt.xlabel('Noggin')
-plt.ylabel('BMP4');
+plt.ylabel('BMP4')
 plt.show()
 
 
-# In[8]:
-
-
 def plot_flow_field(ax, f, u_range, v_range, args=(), n_grid=100):
-    """
+  """
     Plots the flow field with line thickness proportional to speed.
     
     Parameters
@@ -109,34 +105,37 @@ def plot_flow_field(ax, f, u_range, v_range, args=(), n_grid=100):
     output : Matplotlib Axis instance
         Axis with streamplot included.
     """
-    
-    # Set up u,v space
-    u = np.linspace(u_range[0], u_range[1], n_grid)
-    v = np.linspace(v_range[0], v_range[1], n_grid)
-    uu, vv = np.meshgrid(u, v)
 
-    # Compute derivatives
-    u_vel = np.empty_like(uu)
-    v_vel = np.empty_like(vv)
-    for i in range(uu.shape[0]):
-        for j in range(uu.shape[1]):
-            u_vel[i,j], v_vel[i,j] = f(np.array([uu[i,j], vv[i,j]]), None, *args)
+  # Set up u,v space
+  u = np.linspace(u_range[0], u_range[1], n_grid)
+  v = np.linspace(v_range[0], v_range[1], n_grid)
+  uu, vv = np.meshgrid(u, v)
 
-    # Compute speed
-    speed = np.sqrt(u_vel**2 + v_vel**2)
+  # Compute derivatives
+  u_vel = np.empty_like(uu)
+  v_vel = np.empty_like(vv)
+  for i in range(uu.shape[0]):
+    for j in range(uu.shape[1]):
+      u_vel[i, j], v_vel[i, j] = f(np.array([uu[i, j], vv[i, j]]), None, *args)
 
-    # Make linewidths proportional to speed,
-    # with minimal line width of 0.5 and max of 3
-    lw = 0.5 + 2.5 * speed / speed.max()
+  # Compute speed
+  speed = np.sqrt(u_vel**2 + v_vel**2)
 
-    # Make stream plot
-    ax.streamplot(uu, vv, u_vel, v_vel, linewidth=lw, arrowsize=1.2, 
-                  density=1, color='thistle')
+  # Make linewidths proportional to speed,
+  # with minimal line width of 0.5 and max of 3
+  lw = 0.5 + 2.5 * speed / speed.max()
 
-    return ax
+  # Make stream plot
+  ax.streamplot(uu,
+                vv,
+                u_vel,
+                v_vel,
+                linewidth=lw,
+                arrowsize=1.2,
+                density=1,
+                color='thistle')
 
-
-# In[10]:
+  return ax
 
 
 fig, ax = plt.subplots(1, 1)
@@ -148,11 +147,8 @@ ax = plot_flow_field(ax, BMP4_Noggin, (0, 14), (0, 14), args=args)
 plt.show()
 
 
-# In[11]:
-
-
 def plot_traj(ax, f, y0, t, args=(), color='black', lw=2):
-    """
+  """
     Plots a trajectory on a phase portrait.
     
     Parameters
@@ -177,14 +173,13 @@ def plot_traj(ax, f, y0, t, args=(), color='black', lw=2):
     output : Matplotlib Axis instance
         Axis with streamplot included.
     """
-    
-    y = scipy.integrate.odeint(f, y0, t, args=args)
-    ax.plot(*y.transpose(), color=color, lw=lw)
-    return ax
+
+  y = scipy.integrate.odeint(f, y0, t, args=args)
+  ax.plot(*y.transpose(), color=color, lw=lw)
+  return ax
 
 
 # In[12]:
-
 
 fig, ax = plt.subplots(1, 1)
 ax.set_xlabel('Noggin')
@@ -200,4 +195,3 @@ ax = plot_traj(ax, BMP4_Noggin, np.array([1, 0.1]), t, args=args)
 ax = plot_traj(ax, BMP4_Noggin, np.array([1, 1]), t, args=args)
 ax = plot_traj(ax, BMP4_Noggin, np.array([15, 1]), t, args=args)
 plt.show()
-
